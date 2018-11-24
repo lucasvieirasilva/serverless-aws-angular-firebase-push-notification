127b0866c4756078fa9dd1f7b4c34ca852bd7cb0
Lucas Vieira da Silva <lucas.vieira@mjv.com.br>
2018-11-24 16:19:26 -0200

    Change name for serverless client project

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,23 @@
+import config from './config.json';
+
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    messagingSenderId: '387465944169'
+  },
+  apiGateway: {
+    invokeUrl: config.AwsApiGatewayInvokeUrl
+  },
+  amplify: {
+    Auth: {
+      identityPoolId: config.IdentityPoolId,
+      region: config.AwsRegion,
+      userPoolId: config.UserPoolId,
+      userPoolWebClientId: config.UserPoolClientId
+    }
+  },
+  social: {
+    FB: config.FacebookAppId,
+    Google: config.GoogleAppId
+  }
 };
